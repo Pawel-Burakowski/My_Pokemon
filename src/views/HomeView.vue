4addcf2e8371a6c7e2333efa54d71fff813eaf50
Pawel-Burakowski <110766570+Pawel-Burakowski@users.noreply.github.com>
2024-05-28 19:58:35 +0200

    Installation of dependencies

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<h1>Test</h1>
+		<h1>My Pokémon Project</h1>
+		<p>
+			Here you can find some categories to choose from to learn more about the
+			world of Pokémon!
+		</p>
+	</main>
 </template>
+
+<script setup></script>
